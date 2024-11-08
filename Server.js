718f201239a4e6c41b1b3fd562a849236303c207
Sachin1373 <143338039+Sachin1373@users.noreply.github.com>
2024-11-08 23:07:21 +0530

    Server Updated

--- Server.js
+++ Server.js
@@ -15,8 +15,12 @@ const PORT = process.env.PORT || 3030;
 app.use(express.json());
 app.use(cors({
     'origin':'https://auth-frontend-wheat.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     'credentials':true
 }));
+
+app.options('*', cors());
 app.use(express.urlencoded({ extended: true }));
 
 // routes
